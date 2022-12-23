9ada5f20b69059825ce786807d432d179b4adb3f
TGray <gray.tony95@gmail.com>
2022-12-22 20:34:12 -0500

    added foreign key specification to model associations in models/index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const User = require('./User');
 const Post = require('./Post.js');
 
-Post.belongsTo(User);
-User.hasMany(Post);
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
-module.exports = {User, Post}
+module.exports = { User, Post }
