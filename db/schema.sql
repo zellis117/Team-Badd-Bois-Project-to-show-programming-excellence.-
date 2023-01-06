1f15d654dfef882577e87514bea7849659543cd9
Zac Ellis <112399036+zellis117@users.noreply.github.com>
2023-01-05 19:22:04 -0500

    Updated Schema
    Updated schema with state info table.

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS states_db;
+CREATE DATABASE states_db;
+
+USE states_db;
+
+CREATE TABLE states (
+  id INT NOT NULL,
+  state_name VARCHAR(30) NOT NULL,
+  size VARCHAR(30) NOT NULL,
+  fun_fact VARCHAR(250) NOT NULL
+);
