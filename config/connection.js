7c1ad63adda0db967fc839887fd46c439253d01d
TGray <gray.tony95@gmail.com>
2023-01-08 17:00:01 -0500

    updated config.js

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
+  || new Sequelize(process.env.JAWSDB_URL)
+  || new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
